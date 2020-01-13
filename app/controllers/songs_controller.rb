4dd763eca1ad498e30291195a94a8fb9eab9f2aa
Chris Kerbo <chris.kerbo@outlook.com>
2020-01-13 18:01:24 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,13 @@
 class SongsController < ApplicationController
   def index
   		@songs = Song.all
+      
   	end
 
   	def show
   		@song = Song.find(params[:id])
+      @genre = Genre.find(@song.genre_id)
+      @artist = Artist.find(@song.artist_id)
   	end
 
   	def new
